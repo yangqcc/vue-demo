6d8f783405606d5d26d813507bad56fae91d62ae
yangqc <yangqc_ars@outlook.com>
2019-05-27 08:04:34 +0800

    init

---
+++ src/components/App.vue
@@ -0,0 +1,37 @@
+
+<template>
+  <div id="app">
+    <Home></Home>
+  </div>
+</template>
+<script>
+  import Home from "./Home";
+  export default {
+    components: {
+      Home
+    },
+    name: "App"
+  };
+</script>
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+  html,
+  body,
+  #app,
+  .home {
+    height: 100%;
+  }
+  html,
+  body {
+    overflow: hidden;
+  }
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+import menusModule from './module/menusModule'
+Vue.use(Vuex);
+const store = new Vuex.Store({
+  modules: {
+    menusModule
+  }
+})
+export default store;
